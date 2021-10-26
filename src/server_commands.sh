c64047628b5ab6b4b52fa9cc3741e5e8f6edc761
Artem Burtsev <86663451+prometneus@users.noreply.github.com>
2021-10-26 19:33:36 +0300

    Create server_commands.sh

---
+++ src/server_commands.sh
@@ -0,0 +1,24 @@
+ln -s /usr/share/data-minor-bioinf/assemly/oil*
+
+seqtk sample -s1127 oil_R1.fastq 5000000 > sub1.fastq
+seqtk sample -s1127 oil_R2.fastq 5000000 > sub1.fastq
+seqtk sample -s1127 oilMP_S4_L001_R1_001.fastq 1500000 > mp1.fastq
+seqtk sample -s1127 oilMP_S4_L001_R2_001.fastq 1500000 > mp1.fastq
+
+fastqc -o fastqc sub*.fastq mp*.fastq
+multiqc -o multiqc fastqc
+
+platanus_trim sub*.fastq
+platanus_trim mp*.fastq
+
+platanus_internal_trim sub*.fastq
+platanus_internal_trim mp*.fastq
+
+fastqc *.trimmed
+multiqc -o multiqc *.trimmed
+
+rm mp*.fastq sub*.fastq
+
+platanus scaffold -c out_contig.fa -IP1 trimmed/*.trimmed -OP2 *.int_trimmed
+
+platanus gap_close -c out_scaffold.fa -IP1 trimmed/*.trimmed -OP2 *.int_trimmed
